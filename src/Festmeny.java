5c456911a110c7b6e15dc67e7e9e7ae44215bb55
Fuzi18 <fuzesi.t2003@gmail.com>
2022-09-22 08:43:02 +0200

    festmeny

--- src/Festmeny.java
+++ src/Festmeny.java
@@ -1,2 +1,16 @@
-public class Festmeny {
-}
+import java.io.*;
+import java.util.Scanner;
+public class Festmeny
+{
+public static void main(String[] args) throws Exception
+        {
+        Scanner sc = new Scanner(new File("D:\\Programok\\Projekt\\09 13\\AukcioProjekt\\src\\festmenyek.txt"));
+        sc.useDelimiter(",");
+        while (sc.hasNext())
+        {
+        System.out.print(sc.next());
+        }
+        sc.close();
+        }
+        }
+
